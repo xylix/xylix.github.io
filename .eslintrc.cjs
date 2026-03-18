97b28f3ee2d0984e01c568e0948d2db483e899fb
Kerkko Pelttari <kerk.pelt@gmail.com>
2026-03-18 16:00:55 +0200

    Remove unnecessary eslint linting from non-.svelte files since biome now handles that.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,21 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
 	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
 	overrides: [
 		{
 			files: ['*.svelte'],
+			extends: ['plugin:svelte/recommended'],
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
+				parser: '@typescript-eslint/parser',
+				sourceType: 'module',
+				ecmaVersion: 2020,
+				extraFileExtensions: ['.svelte']
+			},
+			env: {
+				browser: true,
+				es2017: true
 			}
 		}
 	]
